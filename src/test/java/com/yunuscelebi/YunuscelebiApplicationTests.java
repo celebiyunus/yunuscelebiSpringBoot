76ba3334e51b862ffa075374dbab2ea8f2a5d833
celebiyunus <yunuscelebiks@gmail.com>
2022-06-03 11:11:22 +0300

    spring boot
    for Spring Boot

---
+++ src/test/java/com/yunuscelebi/YunuscelebiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunuscelebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunuscelebiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
