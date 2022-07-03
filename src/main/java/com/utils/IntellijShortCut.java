098eda1f2684dbf7b3135fbd669fc9bb6736ebdc
celebiyunus <yunuscelebiks@gmail.com>
2022-07-03 21:26:22 +0300

    gitignore (log)

---
+++ src/main/java/com/utils/IntellijShortCut.java
@@ -0,0 +1,10 @@
+package com.utils;
+
+public class IntellijShortCut {
+  /*Settings ==> ctrl + alt + s
+    Alt+ins ==> constructor, toString, getter setter
+    Ctrl+/ ==> single comment
+    Ctrl+shift+/ ==> multiple comment*/
+	
+			
+}
